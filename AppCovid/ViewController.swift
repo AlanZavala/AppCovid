15f2c172bb7dba3c0e7ecf47b0d17264d84da6f8
Alan <alan_fcb10@hotmail.com>
2020-04-16 20:36:40 -0500

    Set up
    App set up

---
+++ AppCovid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppCovid
+//
+//  Created by Alan Zavala on 16/04/20.
+//  Copyright © 2020 Alan Zavala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
